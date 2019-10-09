97df58fe97c8d3efc0090e30909db9adeaf7a657
Kashif Ijaz <fariawan7654@gmail.com>
2019-10-09 10:10:10 +0500

    new update2

---
+++ mylibrary/src/main/java/com/example/mylibrary/Helperjj.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Helperjj {
+
+    Context context;
+
+    public void Helper(){
+        Toast.makeText(context, "This is Helper function", Toast.LENGTH_SHORT).show();
+    }
+}
